536a39ad432f94b4aa2e4bc43c03132a4bed14d6
Nahuel <nahuelgbecerra@gmail.com>
2020-05-21 11:54:47 -0300

    gtm track bubble

--- extra.js
+++ extra.js
@@ -1,4 +1,6 @@
 let btn = document.getElementById("contact")
 btn.addEventListener("click", ()=>{
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({'event': 'clicked_wp_bubble'});
     window.open(" https://api.whatsapp.com/send?phone=[54][1161760576]")
 })
