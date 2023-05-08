77ef3bceb5c42ec918a759909a6b7b04667deb88
dffede87 <dffede87@gmail.com>
2023-05-08 14:30:56 -0300

    added feature federico

--- js/owl.carousel.js
+++ js/owl.carousel.js
@@ -111,7 +111,7 @@ if (typeof Object.create !== "function") {
                 base.transitionTypes(base.options.transitionStyle);
             }
             if (base.options.autoPlay === true) {
-                base.options.autoPlay = 5000;
+                base.options.autoPlay = 1500;
             }
             base.play();
 
